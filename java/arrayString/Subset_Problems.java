9f3d76998da05ca8fc380de71ef6e8beb868ab90
AvinashChander Gottumukkala <gac_0424@yahoo.co.in>
2016-06-22 17:38:14 -0700

    Update Subset_Problems.java

--- java/arrayString/Subset_Problems.java
+++ java/arrayString/Subset_Problems.java
@@ -13,20 +13,22 @@ public class Subset_Problems {
 	}
 
 	//
-	public static void subset_That_Sums_To_N(int[] list, int index, int currSum, int goal, String result)
+	public static void subset_That_Sums_To_N_Print_All(int[] list, int index, int currSum, int goal, String result)
 	{ 
+		//if index goes beyond  array length or currSum is greater than given goal sum, just return
 		if (list.length < index || currSum>goal)
 			return;
+		
+		//iterate thro' array and add each number to currsum and check with goal sum.
 		for (int i = index; i < list.length; i++) {
 
-			currSum = currSum + list[i] ;
-
-			if (currSum == goal)   {
+			if (currSum + list[i]  == goal)   {
 				result =  result + " " + list[i];
 				System.out.println(result);
+				break;
 			}
-			else if (currSum < goal) {
-				subset_That_Sums_To_N(list, i + 1, currSum, goal, result + " " + list[i]);
+			else if (currSum + list[i] < goal) {
+				subset_That_Sums_To_N_Print_All(list, i + 1, currSum + list[i], goal, result + " " + list[i]);
 			}
 		}
 	}
